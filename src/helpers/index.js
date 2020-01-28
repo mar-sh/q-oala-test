f815208f4b1fed625127e296eec7dda0e2df0481
silversea-mar <marasi.samosir@silversea-media.id>
2020-01-28 22:27:08 +0700

    Add docs

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {Object} obj Input Object
+ * @param {Array} shownAttributes Array of attributes to show
+ * @return {Object} Object with attributes that are in shownAttributes array, will return the original input object if no attributes is found
+ */
 export const getAttributesShown = (obj, shownAttributes = []) => {
   const results = {};
 
@@ -7,15 +13,26 @@ export const getAttributesShown = (obj, shownAttributes = []) => {
     }
   }
 
-  return results;
+  return Object.keys(results).length ? results : obj;
 };
 
+/**
+ *
+ * @param {String} key localStorage key name
+ * @return {Object | Array} parsed localStorage item
+ */
 export const getLocalStorageItem = key => {
   const results = JSON.parse(localStorage.getItem(key));
 
   return results;
 };
 
+/**
+ *
+ * @param {String} key localStorage key name
+ * @param {Object | Array} data data input to store in localStorage
+ * @return {null}
+ */
 export const writeToLocalStorage = (key, data) => {
   const JSONdata = JSON.stringify(data);
 
--- src/services/randomUser.js
+++ src/services/randomUser.js
@@ -4,12 +4,18 @@ import config from '../config';
 const { randomUserAPIURL } = config;
 const validateStatus = () => true;
 
+/**
+ * Get random 100 random users from randomUsers API
+ */
 export const getRandomUsers = async () => {
   try {
-    const url = `${randomUserAPIURL}/?results=100`;
+    const url = `${randomUserAPIURL}`;
     const { data: response } = await axios({
       method: 'get',
       url,
+      params: {
+        results: 100,
+      },
       validateStatus,
     });
 
